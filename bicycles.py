1bf7e34e83a9bd780af799421411085bf24ad359
meg-ai-ml <meghanathalakola@gmail.com>
2026-07-31 15:04:55 +0530

    Update bicycles.py

--- bicycles.py
+++ bicycles.py
@@ -9,28 +9,19 @@ Combining list items with strings using f-strings and print arguments.
 
 """
 
+#Using the list function to list different types of bicycles
 bicycles = [ "atlas","ladybird","hero","rockrider"]
-print(bicycles)
+print(bicycles) # ['atlas', 'ladybird', 'hero', 'rockrider']. 
 
-# Name 
-print(bicycles[0])
-print(bicycles[3])
-print(bicycles[2].upper())
-print(bicycles[-1])
-print(bicycles[-2])
+# Listing names of different bicycles using index method.
+print(bicycles[0]) # atlas
+print(bicycles[3]) #rockrider
+print(bicycles[2].upper()) # HERO
+print(bicycles[-1]) # rockrider
+print(bicycles[-2]) # hero
+
+# Printing a message including a certain bicycle using an index method.
+
 message = f"My first bicycle is a {bicycles[0]}"
-print(message)
-
-names = [ "netra","anushri","keyuree","saanvi"]
-print(names)
-print("hello",names[0])
-print("how are you",names[1])
-print("hi",names[2])
-print("howdy",names[3])
-
-transport = [ 'car','bike','scooter']
-print('i want a big',transport[0])
-print('i want a small',transport[1])
-print("i don't want a",transport[2])
-
+print(message) # My first bicycle is a atlas
 
