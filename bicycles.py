43f14302095f17216321fc34d8fb9f7246cbe369
meg-ai-ml <meghanathalakola@gmail.com>
2026-07-31 14:23:42 +0530

    Create bicycles.py with lists and print statements
    Initialize bicycles, names, and transport lists with print statements.

---
+++ bicycles.py
@@ -0,0 +1,27 @@
+# Define a list of four bicycles
+
+bicycles = [ "atlas","ladybird","hero","rockrider"]
+print(bicycles)
+
+# Name 
+print(bicycles[0])
+print(bicycles[3])
+print(bicycles[2].upper())
+print(bicycles[-1])
+print(bicycles[-2])
+message = f"My first bicycle is a {bicycles[0]}"
+print(message)
+
+names = [ "netra","anushri","keyuree","saanvi"]
+print(names)
+print("hello",names[0])
+print("how are you",names[1])
+print("hi",names[2])
+print("howdy",names[3])
+
+transport = [ 'car','bike','scooter']
+print('i want a big',transport[0])
+print('i want a small',transport[1])
+print("i don't want a",transport[2])
+
+
