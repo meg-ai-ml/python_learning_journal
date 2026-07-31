60545c2c7c59ef2aced658a59e7baca60feb5d11
meg-ai-ml <meghanathalakola@gmail.com>
2026-07-31 14:14:06 +0530

    Create animals.py to describe great pets
    Added a program to list three animals and their qualities as pets.

---
+++ animals.py
@@ -0,0 +1,17 @@
+#This short program is to name three animals and why it would be a great pet using for loop.
+
+# Using the list function, we store the name of three animals by declaring the animals variable.
+
+animals = [ "cats", "dogs", "bunnies"]
+
+#Using for loop in order to name an animal and that it would make a great pet.
+
+for animal in animals:
+    print(animal)
+    print("A " + animal + " would make a great pet")
+
+#Using print function, to state that all of these animals would make great pets.
+    
+print("All of these animals would make great pets.")
+
+
