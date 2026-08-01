d93b2de06a7179e33bece4aa7669b5d550fd21eb
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-01 12:32:10 +0530

    Modify buffet tuple elements
    Updated the buffet tuple to include 'baguette' and 'crossiant'.

---
+++ buffet.py
@@ -0,0 +1,45 @@
+#This short program is for creating a tuple.
+
+buffet = ("bread", "butter", "jam", "cereal", "milk" )
+
+#Printing the tuple's output using index method.
+
+print(buffet[0]) #bread
+print(buffet[1]) #butter
+print(buffet[2]) #jam
+print(buffet[3]) #cereal
+print(buffet[4]) #milk
+
+#Printing the tuple's output using for loop.
+
+for food in buffet:
+    print(food)
+    
+"""
+
+bread
+butter
+jam
+cereal
+milk
+
+"""
+
+# Changed couple of elements in tuple.
+
+buffet = ("baguette", "crossiant", "jam", "cereal", "milk" )
+
+#Printing its output using for loop.
+
+for food in buffet:
+    print(food)
+"""
+
+baguette
+crossiant
+jam
+cereal
+milk
+
+"""
+    
