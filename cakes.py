cf58d17e466e51a06c467e4b9084a8a17124a7b6
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 22:45:11 +0530

    Create cakes.py

---
+++ cakes.py
@@ -0,0 +1,74 @@
+# Set the value of the variable.
+cake = "cheesecake"
+
+# Present the prediciton.
+print("Is cake == 'cheesecake'? I predict True.")
+
+# Present if prediction is true or false.
+print( cake == "cheesecake" )
+
+# Present the prediciton.
+print("\nIs cake == 'red velvet'? I predict False. ")
+
+# Present if prediction is true or false.
+print( cake == " red velvet " )
+
+# Set the value of the variable.
+cake = "strawberry"
+
+# Present the prediction.
+print("Is cake == 'strawberry'? I predict True.")
+
+# Present if prediction is true or false.
+print( cake == "strawberry" )
+
+# Present if prediction is true or false.
+print("\nIs cake == 'chocolate'? I predict False. ")
+
+# Present if prediction is true or false.
+print( cake == " chocolate " )
+
+# Set the value of the variable.
+cake = "black forest"
+
+# Present the prediction.
+print("Is cake == 'black forest'? I predict True.")
+
+# Present if prediction is true or false.
+print( cake == "black forest" )
+
+# Present the prediction.
+print("\nIs cake == 'chocolate truffle'? I predict False. ")
+
+# Present if prediction is true or false.
+print( cake == " chocolate truffle " )
+
+# Set the value of the variable.
+cake = "chocolate lava"
+
+# Present the prediction.
+print("Is cake == 'choclate lava'? I predict True.")
+
+# Present if prediction is true or false.
+print( cake == "chocolate lava" )
+
+# Present the prediction.
+print("\nIs cake == 'carrot cake'? I predict False. ")
+
+# Present if prediction is true or false.
+print( cake == " carrot cake " )
+
+# Set the value of the variable.
+cake = "French Silk"
+
+# Present the prediction.
+print("Is cake == 'French Silk'? I predict True.")
+
+# Present if prediction is true or false.
+print( cake == "French Silk" )
+
+# Present the prediction.
+print("\nIs cake == 'blueberry'? I predict False. ")
+
+# Present if prediction is true or false.
+print( cake == " blueberry" )
