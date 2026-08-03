dd0d8f0ca352cc27de85bcd4f27283f45305be39
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 21:39:31 +0530

    Fix comment formatting in alien_colours_success.py

---
+++ alien_colours_success.py
@@ -0,0 +1,6 @@
+# Assign the colour of the alien.
+alien_colour = "pink"
+# Check if the alien is pink and award 5 points.
+if alien_colour == "pink":
+    print("The player has earned 5 points!")
+# The user is presented the statement because the alien is the assigned colour.
