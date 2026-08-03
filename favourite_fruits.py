a95699fdd1a2068b671d8c041e4204de54844fcc
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 22:59:43 +0530

    Add favourite fruits and related print statements

---
+++ favourite_fruits.py
@@ -0,0 +1,16 @@
+favourite_fruits = [ "kiwi", "dragonfruit", "apple"]
+
+if "kiwi" in favourite_fruits:
+    print("Adding kiwi in fruit salad")
+
+if "banana" in favourite_fruits:
+    print("Sorry there is no banana")
+
+if "dragonfruit" in favourite_fruits:
+    print("Adding dragonfruit in fruit salad")
+
+if "chikoo" in favourite_fruits:
+    print("Sorry there is no chikoo")
+
+if "apple" in favourite_fruits:
+    print("Adding apple in fruit salad")   
