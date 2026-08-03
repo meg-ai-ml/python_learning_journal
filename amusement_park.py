b9815edc421d8ca9210768b3eba714e9742fcbc3
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 22:24:51 +0530

    Add admission cost calculation based on age
    Implement age-based admission cost logic.

---
+++ amusement_park.py
@@ -0,0 +1,10 @@
+# Set age of the admit.
+age = 12
+# According to admin's age, allocate the admission cost.
+if age < 4:
+    print("Your admission cost is $0")
+elif age < 18:
+     print("Your admission cost is $25")
+else:
+     print("Your admission cost is $40")
+# The user will be presented the output ($25) according to their age (12).
