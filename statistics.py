24f5c33d4a56d5094f568318d7f793c90e35ec9c
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 23:34:54 +0530

    Add statistics.py to display digit statistics

---
+++ statistics.py
@@ -0,0 +1,7 @@
+digits = list(range(10))
+print(digits)
+
+print("The miniumum digit of this list:  ", min(digits))
+print("The maximum digit of this list:  ", max(digits))
+print("The sum of digits of this list:  ", sum(digits))
+
