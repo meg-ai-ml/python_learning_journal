c512f2edd50b2b04d319ab321982f2b09de77536
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 23:34:16 +0530

    Add age classification based on stages of life

---
+++ stages_of_life.py
@@ -0,0 +1,21 @@
+age = 18
+
+if age < 2 :
+    print("This person is a baby.")
+
+elif 2 < age < 4 :
+    print("This person is a toddler.")
+
+elif 4 < age < 13:
+    print("This person is a kid.")
+    
+
+elif 13 < age < 20 :
+    print("This person is a teenager.")
+
+elif 20 < age < 65 :
+    print("This person is an adult.")
+
+else:
+    print("This person is an elder.")
+    
