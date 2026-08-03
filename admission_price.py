6203db94b0f5b7e8facb51a5855544e76ca0131d
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 23:29:06 +0530

    Create admission_price.py

---
+++ admission_price.py
@@ -0,0 +1,14 @@
+age = 77
+
+if age < 4:
+    admission_price = 0
+elif age < 18:
+    admission_price = 25
+elif age < 65:
+    admission_price = 40
+elif age >= 65:
+    admission_price = 20
+
+
+print("Your admission price is ", admission_price)
+
