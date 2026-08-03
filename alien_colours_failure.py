5b5670efa65b6ef6954c718477f07e4e30b3d420
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 21:44:12 +0530

    Create alien_colours_failure.py

---
+++ alien_colours_failure.py
@@ -0,0 +1,6 @@
+# Assign the colour of the alien.
+alien_colour = "green"
+# Check if the alien is pink  and award 5 points.
+if alien_colour == "pink":
+    print("The player has earned 5 points!")
+
