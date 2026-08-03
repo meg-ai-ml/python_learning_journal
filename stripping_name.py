619775868dc52717180f2ab77a2e06b14ec3160f
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 23:35:32 +0530

    Create stripping_name.py

---
+++ stripping_name.py
@@ -0,0 +1,8 @@
+name = " Viswanatha\tReddy\tThalakola\n "
+print(name)
+print(name.rstrip())
+print(len(name.rstrip()))
+print(name.lstrip())
+print(len(name.lstrip()))
+print(name.strip())
+print(len(name.strip()))
