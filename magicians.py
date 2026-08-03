f7a287b851f72d0d8e3df9044b2b802f1e44e043
meg-ai-ml <meghanathalakola@gmail.com>
2026-08-03 23:16:49 +0530

    Create magicians.py

---
+++ magicians.py
@@ -0,0 +1,8 @@
+magicians = [ "Elise", "David", "Carolina" ]
+
+for magician in magicians:
+    print(magician)
+    
+for magician in magicians:
+    print(f"{magician.upper()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.upper()}\n")
